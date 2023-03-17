d2d802730c0f4b932037b3bbf735e6ee75d2e770
Parabeac-Nest <admin@parabeac.com>
2023-03-17 08:22:06 +0000

    Auto-generated commit by Parabeac Cloud

---
+++ lib/theme/infineit_colors.g.dart
@@ -0,0 +1,22 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';
+class InfineitColors {
+
+static const Color green = Color(0xff04764e);
+
+static const Color black = Color(0xff000000);
+
+static const Color white = Color(0xffffffff);
+
+static const Color dark = Color(0xff373737);
+
+static const Color darkGray = Color(0xff595959);
+
+static const Color gray = Color(0xffcbcbcb);
+}
