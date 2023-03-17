842d4d63be7b89de1bfebf15668a640b18073d99
Parabeac-Nest <admin@parabeac.com>
2023-03-17 08:14:11 +0000

    Auto-generated commit by Parabeac Cloud

---
+++ lib/theme/infineit_colors.g.dart
@@ -0,0 +1,28 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';
+class InfineitColors {
+/// Cod Gray
+static const Color oil11 = Color(0xff0d0d0d);
+
+static const Color absoluteStaticWhiteS = Color(0xffffffff);
+
+static const Color new = Color(0x3d000000);
+/// Cream Can
+static const Color sun05 = Color(0xfff2cf63);
+
+static const Color primaryColor = Color(0xffff9431);
+
+static const Color black = Color(0xff37424f);
+
+static const Color backgroundLightGrey = Color(0xfff0f0f0);
+
+static const Color borderGreyNormal = Color(0xff929292);
+
+static const Color bottomBar = Color(0x807c7676);
+}
---
+++ lib/theme/infineit_text_styles.g.dart
@@ -0,0 +1,93 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';
+class InfineitTextStyles {
+
+static const TextStyle heading2 = TextStyle(
+  fontSize: 50,
+  fontWeight: FontWeight.w700,
+  letterSpacing: 0,
+  fontFamily: 'Poppins',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+/// Semi bold
+
+static const TextStyle heading6 = TextStyle(
+  fontSize: 25,
+  fontWeight: FontWeight.w600,
+  letterSpacing: 0,
+  fontFamily: 'Poppins',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle heading8 = TextStyle(
+  fontSize: 18,
+  fontWeight: FontWeight.w600,
+  letterSpacing: 0,
+  fontFamily: 'Poppins',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle heading7 = TextStyle(
+  fontSize: 23,
+  fontWeight: FontWeight.w600,
+  letterSpacing: 0,
+  fontFamily: 'Poppins',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+/// Bold
+
+static const TextStyle heading5 = TextStyle(
+  fontSize: 30,
+  fontWeight: FontWeight.w700,
+  letterSpacing: 0.6,
+  fontFamily: 'Poppins',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+/// Bold
+
+static const TextStyle heading9 = TextStyle(
+  fontSize: 15,
+  fontWeight: FontWeight.w700,
+  letterSpacing: 0,
+  fontFamily: 'Poppins',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle heading11 = TextStyle(
+  fontSize: 11,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'Poppins',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle heading10 = TextStyle(
+  fontSize: 12,
+  fontWeight: FontWeight.w600,
+  letterSpacing: 0.24,
+  fontFamily: 'Poppins',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+}
